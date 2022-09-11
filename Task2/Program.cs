463602bb04edb526b32f470e95393014057c5851
IvanKovalev <deformgame@gmail.com>
2022-09-11 12:16:10 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+ 
+
+if (k1 * b2 == k2 * b1) 
+    Console.WriteLine("Прямые не пересекаются!");
+else
+{ 
+    double x = -(b1 - b2) / (k1 - k2);
+    double y1 = k1 * x + b1;
+    double y2 = k2 * x + b2;
+    Console.WriteLine($"Точка перечечения линий: ({y1};{y2})");
+}
