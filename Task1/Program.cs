b25ebe7e8a14d5d55984a98d0ca0aa1f7c8f5952
IvanKovalev <deformgame@gmail.com>
2022-09-11 11:05:46 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+Console.Write("Введите несколько чисел через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количество введенных вами элементов которые больше нуля - {count}");
